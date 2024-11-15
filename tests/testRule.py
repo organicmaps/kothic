f25872fc8a1284aa0646932f9533b0735fc68f92
S. Kozyr <s.trump@gmail.com>
2024-11-15 13:16:33 +0200

    Added basic Rule test

--- tests/testRule.py
+++ tests/testRule.py
@@ -5,9 +5,25 @@ from pathlib import Path
 # Add `src` directory to the import paths
 sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
 
+from mapcss.Rule import Rule
+from mapcss.Condition import Condition
+from mapcss import parseCondition
+
 class RuleTest(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_rule(self):
+        rule = Rule()
+        rule.conditions = [
+            parseCondition("aeroway=aerodrome"),
+            parseCondition("aerodrome=international"),
+            Condition("eq", ("::class", "::int_name"))
+        ]
+
+        tt = rule.test({
+            "aeroway": "aerodrome",
+            "aerodrome": "international"
+        })
+        self.assertTrue(tt)
+        self.assertEqual(tt, "::int_name")
 
 if __name__ == '__main__':
     unittest.main()
