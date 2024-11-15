5f86255a2bc0e9592dd4e9e6d4ef44b1c75297bb
S. Kozyr <s.trump@gmail.com>
2024-11-15 15:37:02 +0200

    Added test cases for `cond` function to testEval.py

--- tests/testEval.py
+++ tests/testEval.py
@@ -18,11 +18,13 @@ class EvalTest(unittest.TestCase):
         a = Eval("""eval( prop("dpi") / 2 )""")
         self.assertEqual(a.compute({"lanes": "4"}, {"dpi": 144}), "72")
         self.assertEqual(a.compute({"lanes": "4"}, {"orientation": "vertical"}), "")
+        self.assertSetEqual(a.extract_tags(), set())
 
     def test_eval_num(self):
         a = Eval("""eval( num(tag("lanes")) + 2 )""")
         self.assertEqual(a.compute({"lanes": "4"}), "6")
         self.assertEqual(a.compute({"lanes": "many"}), "2")
+        self.assertSetEqual(a.extract_tags(), {"lanes"})
 
     def test_eval_metric(self):
         a = Eval("""eval( metric(tag("height")) )""")
@@ -38,6 +40,7 @@ class EvalTest(unittest.TestCase):
         self.assertEqual(a.compute({"height": "1200 mm"}), "1.2")
         self.assertEqual(a.compute({"height": "2400MM"}), "2.4")
         self.assertEqual(a.compute({"height": "2800 мм"}), "2.8")
+        self.assertSetEqual(a.extract_tags(), {"height"})
 
     def test_eval_metric_with_scale(self):
         a = Eval("""eval( metric(tag("height")) )""")
@@ -54,6 +57,7 @@ class EvalTest(unittest.TestCase):
         self.assertEqual(a.compute({"height": "1200 mm"}, xscale=4), "4.8")
         self.assertEqual(a.compute({"height": "2400MM"}, xscale=4), "9.6")
         self.assertEqual(a.compute({"height": "2800 мм"}, xscale=4), "11.2")
+        self.assertSetEqual(a.extract_tags(), {"height"})
 
     def test_eval_zmetric(self):
         a = Eval("""eval( zmetric(tag("depth")) )""")
@@ -69,16 +73,19 @@ class EvalTest(unittest.TestCase):
         self.assertEqual(a.compute({"depth": "1200 mm"}), "0.6")
         self.assertEqual(a.compute({"depth": "2400MM"}), "1.2")
         self.assertEqual(a.compute({"depth": "2800 мм"}), "1.4")
+        self.assertSetEqual(a.extract_tags(), {"depth"})
 
     def test_eval_str(self):
         a = Eval("""eval( str( num(tag("width")) - 200 ) )""")
         self.assertEqual(a.compute({"width": "400"}), "200.0")
+        self.assertSetEqual(a.extract_tags(), {"width"})
 
     def test_eval_any(self):
         a = Eval("""eval( any(tag("building"), tag("building:part"), "no") )""")
         self.assertEqual(a.compute({"building": "apartment"}), "apartment")
         self.assertEqual(a.compute({"building:part": "roof"}), "roof")
         self.assertEqual(a.compute({"junction": "roundabout"}), "no")
+        self.assertSetEqual(a.extract_tags(), {"building", "building:part"})
 
     def test_eval_min(self):
         a = Eval("""eval( min( num(tag("building:levels")) * 3, 50) )""")
@@ -86,6 +93,7 @@ class EvalTest(unittest.TestCase):
         self.assertEqual(a.compute({"building:levels": "0"}), "0")
         self.assertEqual(a.compute({"building:levels": "10"}), "30")
         self.assertEqual(a.compute({"building:levels": "30"}), "50")
+        self.assertSetEqual(a.extract_tags(), {"building:levels"})
 
     def test_eval_max(self):
         a = Eval("""eval( max( tag("speed:limit"), 60) )""")
@@ -93,6 +101,16 @@ class EvalTest(unittest.TestCase):
         self.assertEqual(a.compute({"speed:limit": "30"}), "60")
         self.assertEqual(a.compute({"speed:limit": "60"}), "60")
         self.assertEqual(a.compute({"speed:limit": "90"}), "90")
+        self.assertSetEqual(a.extract_tags(), {"speed:limit"})
+
+    def test_eval_cond(self):
+        a = Eval("""eval( cond( boolean(tag("oneway")), 200, 100) )""")
+        self.assertEqual(a.compute({"natural": "wood"}), "100")
+        self.assertEqual(a.compute({"oneway": "yes"}), "200")
+        self.assertEqual(a.compute({"oneway": "no"}), "100")
+        self.assertEqual(a.compute({"oneway": "true"}), "200")
+        self.assertEqual(a.compute({"oneway": "probably no"}), "200")
+        self.assertSetEqual(a.extract_tags(), {"oneway"})
 
     def test_complex_eval(self):
         a = Eval(""" eval( any( metric(tag("height")), metric ( num(tag("building:levels")) * 3), metric("1m"))) """)
