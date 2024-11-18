d7040aea5bcdc7080a55847552cdc895056d212d
S. Kozyr <s.trump@gmail.com>
2024-11-18 22:14:36 +0200

    Added new test cases to testMapCSS.py

--- tests/testMapCSS.py
+++ tests/testMapCSS.py
@@ -5,7 +5,8 @@ from pathlib import Path
 # Add `src` directory to the import paths
 sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
 
-from mapcss import parseDeclaration
+from mapcss import parseDeclaration, MapCSS
+
 
 class MapCSSTest(unittest.TestCase):
     def test_declarations(self):
@@ -35,6 +36,54 @@ class MapCSSTest(unittest.TestCase):
             "pattern-spacing": "90",
         })
 
+    def test_parse_variables(self):
+        parser = MapCSS()
+        parser.parse("""
+@city_label: #999999;
+@country_label: #444444;
+@wave_length: 25;
+""")
+        self.assertEqual(parser.variables, {
+            "city_label": "#999999",
+            "country_label": "#444444",
+            "wave_length": "25"
+        })
+
+    def test_parse_colors(self):
+        parser = MapCSS()
+        parser.parse("""
+@city_label : #999999;
+@country_label: #444444 ;
+  @wave_length: 25;
+""")
+        self.assertEqual(parser.variables, {
+            "city_label": "#999999",
+            "country_label": "#444444",
+            "wave_length": "25"
+        })
+
+    def test_parse_include(self):
+        # TODO: Prepare *.mapcss files with @include(...)
+        pass
+
+    def test_parse_include(self):
+        parser = MapCSS()
+        dynamic_tags = {"tourism": True, "office": True,
+                        "craft": True, "amenity": True}
+        parser.parse("""
+node|z17-[tourism],
+area|z17-[tourism],
+node|z18-[office],
+area|z18-[office],
+node|z18-[craft],
+area|z18-[craft],
+node|z19-[amenity],
+area|z19-[amenity],
+{text: name; text-color: #000030; text-offset: 1;}
+""", dynamic_tags=dynamic_tags)
+
+        self.assertEqual(len(parser.choosers), 1)
+        self.assertEqual(len(parser.choosers[0].ruleChains), 8)
 
 if __name__ == '__main__':
     unittest.main()
