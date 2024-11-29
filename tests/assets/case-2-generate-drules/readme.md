a99d6a981da50acf2148524f0f24fad8c2ca6d6c
S. Kozyr <s.trump@gmail.com>
2024-11-29 09:38:25 +0200

    Removed full style from tests/assets/case-2-generate-drules. Let's use original repo for this test.

--- tests/assets/case-2-generate-drules/readme.md
+++
@@ -1,4 +0,0 @@
-Styles in this folder are borrowed from official Organic Maps repo:
-https://github.com/organicmaps/organicmaps/tree/master/data/styles/default
-
-Files used in testLibkomwm.test_generate_drules() method.
--- tests/testLibkomwm.py
+++ tests/testLibkomwm.py
@@ -11,45 +11,8 @@ from libkomwm import komap_mapswithme
 
 
 class LibKomwmTest(unittest.TestCase):
-    def test_generate_drules(self):
-        assets_dir = Path(__file__).parent / 'assets' / 'case-2-generate-drules'
-
-        class Options(object):
-            pass
-
-        options = Options()
-        options.data = None
-        options.minzoom = 0
-        options.maxzoom = 20
-        options.txt = True
-        options.filename = str( assets_dir / "main.mapcss" )
-        options.outfile = str( assets_dir / "style.bin" )
-        options.priorities_path = str( assets_dir / "include" )
-
-        try:
-            # Save state
-            libkomwm.MULTIPROCESSING = False
-            prio_ranges_orig = deepcopy(libkomwm.prio_ranges)
-            libkomwm.visibilities = {}
-
-            # Run style generation
-            komap_mapswithme(options)
-
-            # Restore state
-            libkomwm.prio_ranges = prio_ranges_orig
-            libkomwm.MULTIPROCESSING = True
-            libkomwm.visibilities = {}
-
-            self.assertTrue(True, "Completed with no errors!")
-        finally:
-            # Clean up generated files
-            files2delete = ["classificator.txt", "colors.txt", "patterns.txt", "style.bin.bin",
-                            "style.bin.txt", "types.txt", "visibility.txt"]
-            for filename in files2delete:
-                (assets_dir / filename).unlink(missing_ok=True)
-
     def test_generate_drules_mini(self):
-        assets_dir = Path(__file__).parent / 'assets' / 'case-3-generate-drules-mini'
+        assets_dir = Path(__file__).parent / 'assets' / 'case-2-generate-drules-mini'
 
         class Options(object):
             pass
@@ -97,7 +60,7 @@ class LibKomwmTest(unittest.TestCase):
                 (assets_dir / filename).unlink(missing_ok=True)
 
     def test_generate_drules_validation_errors(self):
-        assets_dir = Path(__file__).parent / 'assets' / 'case-4-styles-validation'
+        assets_dir = Path(__file__).parent / 'assets' / 'case-3-styles-validation'
         # TODO: needs refactoring of libkomwm.validation_errors_count to have a list
         #       of validation errors.
         self.assertTrue(True)
